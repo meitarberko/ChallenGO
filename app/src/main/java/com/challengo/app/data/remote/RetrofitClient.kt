91865cbea6b38292acac69af88147471c2e69e28
meitarberko <meitar.be@gmail.com>
2026-03-15 21:33:03 +0200

    feat(remote): add retrofit client

---
+++ app/src/main/java/com/challengo/app/data/remote/RetrofitClient.kt
@@ -0,0 +1,30 @@
+package com.challengo.app.data.remote
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+    private const val BASE_URL = "https://zenquotes.io/api/"
+    
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        HttpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+    }
+    
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .build()
+    
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+    
+    val zenQuotesApi: ZenQuotesApi = retrofit.create(ZenQuotesApi::class.java)
+}
